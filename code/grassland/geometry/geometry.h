7b5e779d6989a22e241768e23498b0da8908ecb3
LazyJazz <zijian.lv@hotmail.com>
2024-05-28 16:22:02 +0800

    Temporal save

--- code/grassland/geometry/geometry.h
+++ code/grassland/geometry/geometry.h
@@ -3,4 +3,5 @@
 #include "grassland/geometry/axis_aligned_bounding_box.h"
 #include "grassland/geometry/continuous_collision_detection.h"
 #include "grassland/geometry/ray.h"
+#include "grassland/geometry/spd_project.h"
 #include "grassland/geometry/triangle.h"
