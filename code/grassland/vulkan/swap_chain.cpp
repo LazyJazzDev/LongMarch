809124e3cca01ee74e9b9d0633e04b2b574b4b17
LazyJazz <zijian.lv@hotmail.com>
2025-06-09 16:35:57 +0800

    [upd][vulkan] Make no v-sync default option

--- code/grassland/vulkan/swap_chain.cpp
+++ code/grassland/vulkan/swap_chain.cpp
@@ -89,7 +89,7 @@ VkSurfaceFormatKHR Swapchain::ChooseSwapSurfaceFormat(const std::vector<VkSurfac
 
 VkPresentModeKHR Swapchain::ChooseSwapPresentMode(const std::vector<VkPresentModeKHR> &availablePresentModes) {
   for (const auto &availablePresentMode : availablePresentModes) {
-    if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+    if (availablePresentMode == VK_PRESENT_MODE_IMMEDIATE_KHR) {
       return availablePresentMode;
     }
   }
